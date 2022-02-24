b53c70d95205113bb1c2adb9fd222f8c91a42e45
armandios19 <fooseaov@gmail.com>
2022-02-24 12:12:41 -0600

    grafico de edad y region

--- scripts/funciones_modificadas_01.R
+++ scripts/funciones_modificadas_01.R
@@ -30,6 +30,7 @@ region_plot <- function(region, title, title_alig = 0.5){
 # clasificacion por region y estrato socioeconomico
 
 # se deben normalizar estos datos
+# hacer grafico que solo obtenga datos por estrato
 
 socioeconomic_plot <- function (region, title, title_alig = 0.5){
   ensanut_filtrado %>%
@@ -45,6 +46,59 @@ socioeconomic_plot <- function (region, title, title_alig = 0.5){
           axis.ticks.x=element_blank())+
     ggtitle(title)+
     labs(fill = "Estrato sociodemográfico")+
+    ylab("Frecuencia de consumo")+
+    xlab("")+
+    facet_grid(.~alimentos, switch = "x")+
+    theme(plot.title = element_text(hjust = title_alig))+
+    theme(strip.text.x = element_text(size = 10, angle = 90, hjust = 1),
+          panel.border=element_blank(),
+          strip.background=element_rect(colour="white", fill="white"))
+
+}
+
+# clasificacion por sexo
+
+sex_region_plot <- function (region, title, title_alig = 0.5){
+  ensanut_filtrado %>%
+    group_by(sexo) %>%
+    select(alimentos, sexo, zona) %>%
+    filter(zona == region) %>% count() %>%
+    ggplot(aes(x=sexo, y=freq, fill=sexo))+
+    geom_bar(stat = "identity")+
+    theme_minimal()+
+    scale_fill_manual(values = c("lightblue4", "salmon2"))+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())+
+    ggtitle(title)+
+    labs(fill = "Sexo")+
+    ylab("Frecuencia de consumo")+
+    xlab("")+
+    facet_grid(.~alimentos, switch = "x")+
+    theme(plot.title = element_text(hjust = title_alig))+
+    theme(strip.text.x = element_text(size = 10, angle = 90, hjust = 1),
+          panel.border=element_blank(),
+          strip.background=element_rect(colour="white", fill="white"))
+
+}
+
+
+# clasificacion por edad
+
+age_plot <- function (region, title, title_alig = 0.5){
+  ensanut_filtrado %>%
+    group_by(edades) %>%
+    select(alimentos, edades, zona) %>%
+    filter(zona == region) %>% count() %>%
+    ggplot(aes(x=edades, y=freq, fill=edades))+
+    geom_bar(stat = "identity")+
+    theme_minimal()+
+    scale_fill_manual(values = c("turquoise3", "yellow4", "orchid4"))+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())+
+    ggtitle(title)+
+    labs(fill = "Edades")+
     ylab("Frecuencia de consumo")+
     xlab("")+
     facet_grid(.~alimentos, switch = "x")+
